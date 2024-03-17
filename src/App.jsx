de685189ba118a9c823a9b237ffddb4bec36f647
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-03-17 16:11:33 +0600

    currently cooking section updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ import Cooking from "./Components/currentCooking/Cooking"
 
 
 function App() {
+
 const [card,setCard] = useState([]);
 const [cookItem,setCookItem] = useState(0);
 const [prepare,setPrepare] = useState([]);
 const [cooked,setCooked] = useState(0);
+const [cookTime,setTime] = useState(0);
+const [calories,setCalories] = useState(0);
 
 function handleCard(item){
  const isExist = card.find(i=>i.recipe_id == item.recipe_id) 
@@ -30,11 +33,26 @@ function handleCard(item){
  
 }
 
-function handlePrepare(item){
-  setPrepare([...prepare,item]);
-  setCooked(cooked+1)
+function handlePrepare(item,item2,item3){
+  const filter = prepare.filter(i=>i.recipe_id != item);
+
+  setCard(filter)
+  setPrepare([...filter,item]);
+
+  setCooked(cooked + 1);
+  setCookItem(cookItem - 1);
+
+  let convertedTime = parseInt(item3.slice(0,3));
+  let convertedCalories = parseInt(item2.slice(0,3));
+
+  let time = cookTime + convertedTime;
+  let newCalories = calories + convertedCalories;
+  setTime(time);
+  setCalories(newCalories);
+
 }
-console.log(prepare);
+
+
   return (
     <>
      <Header></Header>
@@ -57,8 +75,12 @@ console.log(prepare);
           <Cooking cooked={cooked}></Cooking>
 
          {
-          prepare.map((i,index)=><Prepare key={i.recipe_id} item={i} index={index}></Prepare>)
+          prepare.map((item,index)=><Prepare key={item.recipe_id} item={item} index={index}></Prepare>)
          }
+         <div className="text-xl font-bold text-right">
+          <p>Total Time: {cookTime}</p>
+          <p>Total Calories: {calories}</p>
+         </div>
         </div>  
      </div>
      <ToastContainer></ToastContainer>
--- src/Components/RightSideCards/RightSide.jsx
+++ src/Components/RightSideCards/RightSide.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function RightSide({item,index,handlePrepare}) {
   return (
     <div className="bg-slate-200 mb-3">
@@ -6,7 +8,7 @@ export default function RightSide({item,index,handlePrepare}) {
         <p>{item.recipe_name}</p>
           <p>{item.preparing_time}</p>
           <p>{item.calories}</p>
-          <button onClick={()=>handlePrepare(item)} className="px-5 py-2 bg-[#0be58a] rounded-full mr-5">Preparing</button>
+          <button onClick={()=>handlePrepare(item,item.calories,item.preparing_time)} className="px-5 py-2 bg-[#0be58a] rounded-full mr-5">Preparing</button>
         </div>
     </div>
   )
